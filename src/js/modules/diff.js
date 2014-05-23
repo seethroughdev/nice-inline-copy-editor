e5bcf0902b1ddd1edb83b0bfd422a2aab6663d55
Adam L <github@adamin.net>
2014-05-23 16:04:10 -0700

    Added diff module

---
+++ src/js/modules/diff.js
@@ -0,0 +1,17 @@
+var $ = window.jQuey || require('domtastic')
+,   jsdiff = require('diff')
+,   content = require('./content');
+
+
+var diffObj = {
+
+  init: function() {
+
+  },
+
+
+
+}
+
+
+module.exports = diffObj;
