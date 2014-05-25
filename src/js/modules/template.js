f40e485b2fea9ef0aeb6d4a2856309583484c338
Adam L <github@adamin.net>
2014-05-24 17:25:59 -0700

    CSS improvements

--- src/js/modules/template.js
+++ src/js/modules/template.js
@@ -3,7 +3,7 @@
 // set objTemplate
 var objTemplate = '<ul id="nice-nav">';
     objTemplate += '<li id="nice-title" title="Go To Homepage">NICE</li>';
-    objTemplate += '<li id="nice-min" title="Minimize NICE">\uE001</li>';
+    objTemplate += '<li id="nice-min" title="Minimize NICE"><span>\uE001</span></li>';
     objTemplate += '<li id="nice-off" title="Turn off NICE">\uE003</li>';
     objTemplate += '<li id="nice-diff" title="See Diff">\uE002</li>';
     objTemplate += '<li id="nice-toggle" title="Toggle Original">\uE004</li>';
